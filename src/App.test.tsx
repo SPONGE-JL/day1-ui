39b22228f29eb2bcd2939ca62477447afa97d2d9
SPONGE-JL <dev2sponge@gmail.com>
2022-12-24 10:27:48 +0900

    ui: eject unncessary component

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders basic source", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, React-TS!/i);
-  expect(linkElement).toBeInTheDocument();
-});
